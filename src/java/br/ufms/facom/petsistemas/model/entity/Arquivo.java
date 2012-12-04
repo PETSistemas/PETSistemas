ba9589aac174da1937a0f3342fb5e09479fe1256
rkuninari <rkuninari@A2591.detran.ms.gov.br>
2012-12-04 12:35:19 -0200

    Criação e Mapeamento das Classes: -Pessoa -Tutor -Petiano -Colaborador -Arquivo

---
+++ src/java/br/ufms/facom/petsistemas/model/entity/Arquivo.java
@@ -0,0 +1,134 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufms.facom.petsistemas.model.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author rkuninari
+ */
+@Entity
+@Table(name = "ARQUIVO")
+public class Arquivo implements Serializable
+{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false, nullable = false, unique = true)
+    private Long id;
+
+    @Column(name = "caminho", updatable = true, nullable = false)
+    private String caminho;
+
+    @Column(name = "dataCriacao", updatable = false, nullable = false)
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date dataCriacao;
+
+    @Column(name = "tamanho", updatable = true, nullable = false)
+    private int tamanho;
+
+    @Column(name = "titulo", updatable = true, nullable = false)
+    private String titulo;
+
+    @Column(name = "categoria", updatable = true, nullable = false)
+    private int categoria;
+
+    @Column(name = "tipo", updatable = true, nullable = false)
+    private int tipo;
+
+    @OneToOne(mappedBy = "arquivo")
+    private Pessoa pessoa;
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    public String getCaminho()
+    {
+        return caminho;
+    }
+
+    public void setCaminho(String caminho)
+    {
+        this.caminho = caminho;
+    }
+
+    public Date getDataCriacao()
+    {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao)
+    {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public int getTamanho()
+    {
+        return tamanho;
+    }
+
+    public void setTamanho(int tamanho)
+    {
+        this.tamanho = tamanho;
+    }
+
+    public String getTitulo()
+    {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo)
+    {
+        this.titulo = titulo;
+    }
+
+    public int getCategoria()
+    {
+        return categoria;
+    }
+
+    public void setCategoria(int categoria)
+    {
+        this.categoria = categoria;
+    }
+
+    public int getTipo()
+    {
+        return tipo;
+    }
+
+    public void setTipo(int tipo)
+    {
+        this.tipo = tipo;
+    }
+
+    public Pessoa getPessoa()
+    {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa)
+    {
+        this.pessoa = pessoa;
+    }
+
+}
