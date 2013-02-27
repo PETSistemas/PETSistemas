ed3a3d8b963f48ce8b7c3e139dbe380c839dc106
brunogok <brunogok@hotmail.com>
2013-02-27 19:28:30 -0300

    Arrumar com colaborador e tutor

--- src/java/br/ufms/facom/petsistemas/controller/tutor/TutorServlet.java
+++ src/java/br/ufms/facom/petsistemas/controller/tutor/TutorServlet.java
@@ -5,6 +5,9 @@
 package br.ufms.facom.petsistemas.controller.tutor;
 
 import br.ufms.facom.petsistemas.controller.Utilitarios;
+import br.ufms.facom.petsistemas.model.entity.Petiano;
+import br.ufms.facom.petsistemas.model.dao.petiano.PetianoDAO;
+import br.ufms.facom.petsistemas.model.dao.petiano.PetianoDAOImplementacao;
 import br.ufms.facom.petsistemas.model.dao.tutor.TutorDAO;
 import br.ufms.facom.petsistemas.model.dao.tutor.TutorDAOImplementacao;
 import br.ufms.facom.petsistemas.model.entity.Tutor;
@@ -25,6 +28,7 @@ import javax.servlet.http.HttpServletResponse;
 public class TutorServlet extends HttpServlet {
 
     TutorDAO controladorBD;
+    PetianoDAO controladorPetianoBD;
 
     /**
      * Método executado na inicialização da Classe
@@ -37,7 +41,7 @@ public class TutorServlet extends HttpServlet {
     }
 
     /**
-     * Método responsável pela pesquisa de todos os tutors cadastrados
+     * Método responsável pela pesquisa de todos os tutores cadastrados
      *
      * @param request
      */
@@ -51,36 +55,86 @@ public class TutorServlet extends HttpServlet {
      *
      * @param request
      */
-    private void salvarTutor(HttpServletRequest request) {
+    public boolean validarInclusao(HttpServletRequest request) {
         String nome = request.getParameter("nome");
         String dataNasc = request.getParameter("dataNascimento");
-        Date dataNascimento = Utilitarios.stringParaData(dataNasc);
         String email = request.getParameter("email");
         String lattes = request.getParameter("lattes");
         String rg = request.getParameter("rg");
         String cpf = request.getParameter("cpf");
-        String senha = request.getParameter("senha");
+        String senha = Utilitarios.encripta(request.getParameter("senha"));
         String dataEnt = request.getParameter("dataEntrada");
-        Date dataEntrada = Utilitarios.stringParaData(dataEnt);
         String dataSai = request.getParameter("dataSaida");
+
+        if (!Utilitarios.isCPF(cpf)) {
+            request.setAttribute("mensagemErroTutor", "O número: " + cpf +" não é um CPF válido!");
+            return false;
+        } else if (nome.equals("") || nome == null) {
+            request.setAttribute("mensagemErroTutor", "O Campo Nome é Obrigatógio!");
+            return false;
+        } else if (dataNasc.equals("") || dataNasc == null) {
+            request.setAttribute("mensagemErroTutor", "O Campo Data de Nascimento é Obrigatógio!");
+            return false;
+        } else if (rg.equals("") || rg == null) {
+            request.setAttribute("mensagemErroTutor", "O Campo RG é Obrigatógio!");
+            return false;
+        } else if (cpf.equals("") || cpf == null) {
+            request.setAttribute("mensagemErroTutor", "O Campo CPF é Obrigatógio!");
+            return false;
+        } else if (email.equals("") || email == null) {
+            request.setAttribute("mensagemErroTutor", "O Campo Nome é Obrigatógio!");
+            return false;
+        } else if (lattes.equals("") || lattes == null) {
+            request.setAttribute("mensagemErroTutor", "O Campo Link do Curriculum Lattes é Obrigatógio!");
+            return false;
+        } else if (dataEnt.equals("") || dataEnt == null) {
+            request.setAttribute("mensagemErroTutor", "O Campo Data de Entrada é Obrigatógio!");
+            return false;
+        } else {
+            buscarTutorCPF(request);
+            controladorPetianoBD = new PetianoDAOImplementacao();
+            Petiano petiano = controladorPetianoBD.buscarPetianoPeloCPF(cpf);
+            if (request.getAttribute("tutorBusca") != null) {
+                request.setAttribute("mensagemErroTutor", "Já existe um tutor com este CPF!");
+                return false;
+            } else if (petiano != null) {
+                request.setAttribute("mensagemErroTutor", "Já existe um tutor com este CPF!");
+                return false;
+            } else {
+                Date dataNascimento = Utilitarios.stringParaData(dataNasc);
+                Date dataEntrada = Utilitarios.stringParaData(dataEnt);
+                salvarTutor(request, senha, rg, cpf, dataEntrada, dataSai, nome, dataNascimento, email, lattes);
+                return true;
+            }
+        }
+    }
+
+    /**
+     * Método responsável pela criação de um novo tutor
+     *
+     * @param request
+     */
+    private void salvarTutor(HttpServletRequest request, String senha, String rg, String cpf, Date dataEntrada, String dataSai, String nome, Date dataNascimento, String email, String lattes) {
 
         Date dataSaida = null;
 
-        if (dataSai != null) {
+        if (!dataSai.equals("")) {
             dataSaida = Utilitarios.stringParaData(dataSai);
         }
 
         Tutor tutor = new Tutor(senha, rg, cpf, dataEntrada, dataSaida, nome, dataNascimento, email, lattes, null);
-        tutor.setDataEntradaFormatada(dataEnt);
-        tutor.setDataNascimentoFormatada(dataNasc);
-        if (dataSai != null) {
+        tutor.setDataEntradaFormatada(Utilitarios.dataParaString(dataEntrada));
+        tutor.setDataNascimentoFormatada(Utilitarios.dataParaString(dataNascimento));
+
+        if (!dataSai.equals("")) {
             tutor.setDataSaidaFormatada(dataSai);
+        } else {
+            tutor.setDataSaidaFormatada(null);
         }
         controladorBD.inserir(tutor);
-        request.setAttribute("mensagem", "Tutor " + nome + " cadastrado com sucesso!");
     }
 
-    private void atualizarTutor(HttpServletRequest request) {
+   public boolean validarEdição(HttpServletRequest request) {
 
         String nome = request.getParameter("nome");
         String dataNasc = request.getParameter("dataNascimento");
@@ -88,25 +142,54 @@ public class TutorServlet extends HttpServlet {
         String email = request.getParameter("email");
         String lattes = request.getParameter("lattes");
         String rg = request.getParameter("rg");
+        String endereco = request.getParameter("endereco");
         String dataEnt = request.getParameter("dataEntrada");
         Date dataEntrada = Utilitarios.stringParaData(dataEnt);
         String dataSai = request.getParameter("dataSaida");
+
+
+        if (nome.equals("") || nome == null) {
+            request.setAttribute("mensagemErroPetiano", "O Campo Nome é Obrigatógio!");
+            return false;
+        } else if (dataNasc.equals("") || dataNasc == null) {
+            request.setAttribute("mensagemErroPetiano", "O Campo Data de Nascimento é Obrigatógio!");
+            return false;
+       } else if (rg.equals("") || rg == null) {
+            request.setAttribute("mensagemErroPetiano", "O Campo RG é Obrigatógio!");
+            return false;
+        } else if (email.equals("") || email == null) {
+            request.setAttribute("mensagemErroPetiano", "O Campo Nome é Obrigatógio!");
+            return false;
+        } else if (lattes.equals("") || lattes == null) {
+            request.setAttribute("mensagemErroPetiano", "O Campo Link do Curriculum Lattes é Obrigatógio!");
+            return false;
+       } else if (dataEnt.equals("") || dataEnt == null) {
+            request.setAttribute("mensagemErroPetiano", "O Campo Data de Entrada é Obrigatógio!");
+            return false;
+        } else {
+            atualizarTutor(request, rg, dataEntrada, dataSai, nome, dataNascimento, email, lattes);
+            return true;
+        }
+    }
+
+    private void atualizarTutor(HttpServletRequest request, String rg, Date dataEntrada, String dataSai, String nome, Date dataNascimento, String email, String lattes) {
+
         Date dataSaida = null;
-        if (dataSai != null) {
+
+        if (!dataSai.equals("")) {
             dataSaida = Utilitarios.stringParaData(dataSai);
         }
-        Long id = Long.parseLong(request.getParameter("id"));
 
+        Long id = Long.parseLong(request.getParameter("id"));
         Tutor tutor = controladorBD.retrieve(id);
 
         tutor.setNome(nome);
         tutor.setDataEntrada(dataEntrada);
         tutor.setDataNascimento(dataNascimento);
-        
         tutor.setDataSaida(dataSaida);
         tutor.setDataEntradaFormatada(Utilitarios.dataParaString(dataEntrada));
         if (dataSaida != null) {
-            tutor.setDataSaidaFormatada(Utilitarios.dataParaString(dataSaida));
+            tutor.setDataSaidaFormatada(dataSai);
         } else {
             tutor.setDataSaidaFormatada("");
         }
@@ -120,7 +203,6 @@ public class TutorServlet extends HttpServlet {
 
         request.setAttribute("mensagem", "Tutor " + nome + " cadastrado com sucesso!");
     }
-
     /**
      * Método responsável pela busca de Tutor pelo CPF
      *
@@ -133,14 +215,6 @@ public class TutorServlet extends HttpServlet {
 
         request.setAttribute("tutorBusca", tutor);
 
-        request.setAttribute("nome", tutor.getNome());
-        request.setAttribute("dataNascimento", Utilitarios.dataParaString(tutor.getDataNascimento()));
-        request.setAttribute("email", tutor.getEmail());
-        request.setAttribute("lattes", tutor.getLinkLattes());
-        request.setAttribute("senha", tutor.getSenha());
-        request.setAttribute("rg", tutor.getRg());
-        request.setAttribute("cpf", tutor.getCpf());
-        request.setAttribute("dataEntrada", Utilitarios.dataParaString(tutor.getDataEntrada()));
     }
 
     /**
@@ -214,19 +288,23 @@ public class TutorServlet extends HttpServlet {
 
         } else if (request.getRequestURI()
                 .endsWith("/editarTutor")) {
-            if (sessaoEstaAtiva(request)) {
+            if (Utilitarios.sessaoEstaAtiva(request, "login")) {
                 buscarTutorCPF(request);
                 pagina = "editarTutor";
             }
 
         } else if (request.getRequestURI()
                 .endsWith("/atualizarTutor")) {
-            if (sessaoEstaAtiva(request)) {
-                atualizarTutor(request);
-                listarTutores(request);
+            if (Utilitarios.sessaoEstaAtiva(request, "login")) {
+                if (validarEdição(request)) {
+                    listarTutores(request);
+                } else {
+                    pagina = "editarTutor";
+                }
+
             }
         } else if (request.getRequestURI().endsWith("/novoTutor")) {
-            iniciarSinal(request);
+            Utilitarios.iniciarSinal(request);
             pagina = "novoTutor";
 
         } else if (request.getRequestURI()
@@ -236,17 +314,20 @@ public class TutorServlet extends HttpServlet {
 
         } else if (request.getRequestURI()
                 .endsWith("/salvarTutor")) {
-            if (sessaoEstaAtiva(request)) {
-                if (sinalOK(request)) {
-                    salvarTutor(request);
-                    apagarSinal(request);
+            if (Utilitarios.sessaoEstaAtiva(request, "login")) {
+                if (Utilitarios.sinalOK(request)) {
+                    if (validarInclusao(request)) {
+                        Utilitarios.apagarSinal(request);
+                        listarTutores(request);
+                    } else {
+                        pagina = "novoTutor";
+                    }
                 }
-                listarTutores(request);
             }
 
         } else if (request.getRequestURI()
                 .endsWith("/removerTutor")) {
-            if (sessaoEstaAtiva(request)) {
+            if (Utilitarios.sessaoEstaAtiva(request, "login")) {
                 removerTutor(request);
             }
             listarTutores(request);
@@ -264,8 +345,8 @@ public class TutorServlet extends HttpServlet {
         request.getRequestDispatcher(
                 "/index.jsp").forward(request, response);
 
-    }
 
+    }
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP
